f0725035ac411ceb19c7602f3e05e0187cffd4ef
Jake Worth <jake@jakeworth.com>
2017-12-16 19:03:01 -0600

    Add logic for score-based colors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ScoreTable />
       </div>
     );
   }
 }
+
+const ScoreTable = () => {
+  const scoreTree = [
+    [0, 1, 2, 3, 0],
+    [0, 1, 2, 3, 1],
+    [0, 4, 2, 3, 0],
+    [0, 1, 2, 3, 5],
+    [5, 1, 2, 3, 0],
+  ];
+
+  const scoreToColor = score => {
+    switch (score) {
+      case 0:
+        return 'red';
+      case 1:
+        return 'yellow';
+      case 2:
+        return 'green';
+      case 3:
+        return 'blue';
+      case 4:
+        return 'purple';
+      default:
+        return 'black';
+    }
+  };
+
+  const scoreRow = index => {
+    return scoreTree[index].map(score => {
+      return <td style={{ background: scoreToColor(score) }} />;
+    });
+  };
+
+  return (
+    <table
+      style={{
+        margin: '0 auto',
+        border: '2px solid black',
+        width: '500px',
+        height: '500px',
+      }}
+    >
+      <tr>{scoreRow(0)}</tr>
+      <tr>{scoreRow(1)}</tr>
+      <tr>{scoreRow(2)}</tr>
+      <tr>{scoreRow(3)}</tr>
+      <tr>{scoreRow(4)}</tr>
+    </table>
+  );
+};
 
 export default App;
